9fd577b85fb7e09940334dd53be5bad188a94d92
kuriwaki0419 <ryuto614182@gmail.com>
2023-05-24 09:58:41 +0900

    jsの修正

--- display/logincode.js
+++ display/logincode.js
@@ -1,6 +1,13 @@
 $("#login-button").click(function(event){
-    event.preventDefault();
-
+/*    event.preventDefault();*/
+/*
 $('form').fadeOut(500);
 $('.wrapper').addClass('form-success');
+});
+
+*/
+if ($('form')[0].checkValidity()) {
+    $('form').fadeOut(500);
+    $('.wrapper').addClass('form-success');
+}
 });
